3bb8f917e8827d75ebee451c444204732e5da3ae
ruffy <ruffy.developer@gmail.com>
2020-04-21 09:55:12 +0200

    Make compileIndex.sh generate a working html.

--- compileIndex.sh
+++ compileIndex.sh
@@ -1,9 +1,19 @@
+source_path=~/ProgrammingDiary
 
-cd /source
+current_path=$PWD
+cd $source_path
 
+source ~/code/spin/archetype-base/import.sh
+
+export DEFAULT_TEMPLATES_PATH=$current_path/templates
+export OVERRIDE_TEMPLATES_PATH=/override/templates
+export SOURCE_ROOT=$source_path
+export TARGET_ROOT=./target
 
 ls *.md | extract-metadata.js | \
   map-inline.js -p title 'cat ${.filename} | extract-title.js' | \
-  map-inline.js -p ingress 'cat ${.filename} | extract-ingress.js' 
+  map-inline.js -p ingress 'cat ${.filename} | extract-ingress.js' | \
+  jqwrap.js blogTeasers | \
+  handlebars.js --inline true index | handlebars.js main
 
 
